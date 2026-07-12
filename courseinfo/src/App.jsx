35965f86edbdc0ab88932bde58c987eb4f797790
Maaria Tiiri <maaria.tiiri@aalto.fi>
2026-07-12 22:00:39 +0300

    2.5 done

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,38 +1,4 @@
-
-const Course = (props) => {
-  const {course} = props
-  return (
-  <div key={course.id}>
-    <Header name={course.name}/>
-    <Content parts={course.parts}/>
-  </div>
-)
-}
-
-const Header = (props) => <h1>{props.name}</h1>
-
-const Content = (props) => {
-  const {parts} = props
-  const init = 0
-  const total = parts.reduce((acc, curr) => acc+curr.exercises, init)
-  return (
-    <div>
-      {parts.map(pt => <Part part={pt} key={pt.id}/>)}
-      <strong>total of {total} excercises</strong>
-    </div>
-  )
-}
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-)
-
-const Total = (props) => <p>Number of exercises {props.total}</p>
-
-
-
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
