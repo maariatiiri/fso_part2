4ab7964a280ac69a9f896f8b2103bed416ab6ec1
Maaria Tiiri <maaria.tiiri@aalto.fi>
2026-07-20 12:33:37 +0300

    2.11 done

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Person = ({person}) => <p>{person.name} {person.number}</p>
 
@@ -6,22 +7,20 @@ const Filter = ({value, handler}) => {
   return (
     <form>
       <div>
-        filter shown with <Input value={value} handler={handler}/>
+        filter shown with <input value={value} onChange={handler}/>
       </div>
     </form>
   )
 }
 
-const Input = ({value, handler}) => <input value={value} onChange={handler}/>
-
 const PersonForm = ({newName, nameHandler, newNumber, numberHandler, onSubmit}) => {
   return(
     <form onSubmit={onSubmit}>
       <div>
-        name: <Input value={newName} handler={nameHandler}/>
+        name: <input value={newName} onChange={nameHandler}/>
       </div>
       <div>
-        number: <Input value={newNumber} handler={numberHandler}/>
+        number: <input value={newNumber} onChange={numberHandler}/>
       </div>
       <div>
         <button type="submit">add</button>
@@ -39,16 +38,16 @@ const Persons = ({personsToShow}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data))
+  }, [])
 
   const personsToShow = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
 
@@ -66,17 +65,11 @@ const App = () => {
     
   }
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+  const handleNameChange = (event) => setNewName(event.target.value)
 
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
 
-  const handleFilterChange = (event) => {
-    setFilter(event.target.value)
-  }
+  const handleFilterChange = (event) => setFilter(event.target.value)
 
   return (
     <div>
